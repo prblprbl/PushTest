2795ecc3ce07c9f230ab2d0626ee39090f509a10
prbl <tlrks2008@naver.com>
2023-02-22 17:40:14 +0900

    Commit 2 Canny 알고리즘 추가

--- photo.py
+++ photo.py
@@ -2,9 +2,10 @@ import cv2
 
 src = cv2.imread("c:/images/picture01.jpg")
 
-dst = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+#dst = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
-
+dst = cv2.cvtColor(src, cv2.IMREAD_GRAYSCALE)
+dst = cv2.Canny(dst, 100, 200, apertureSize=3, L2gradient=True)
 
 
 cv2.imshow("", src)
