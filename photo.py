134148ad5aa073ab8f9c05827661082cba5a9ab0
prbl <tlrks2008@naver.com>
2023-02-22 17:44:23 +0900

    Commit 3 원본과 수정사진 두 사진 띄우도록 수정

--- photo.py
+++ photo.py
@@ -8,8 +8,8 @@ dst = cv2.cvtColor(src, cv2.IMREAD_GRAYSCALE)
 dst = cv2.Canny(dst, 100, 200, apertureSize=3, L2gradient=True)
 
 
-cv2.imshow("", src)
-cv2.imshow("", dst)
+cv2.imshow("1", src)
+cv2.imshow("2", dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
